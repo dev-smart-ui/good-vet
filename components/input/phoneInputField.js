6c89f1a1dbb1681511c489b4b8a69ec2dbf8ff87
AlexP <86realmadrid86@gmail.com>
2023-08-20 20:56:00 +0300

    install new library for the phone input, fixed errors and bugs

--- components/input/phoneInputField.js
+++ components/input/phoneInputField.js
@@ -1,24 +1,7 @@
-import {forwardRef} from "react"
 import styles from "../registration/form/registerForm.module.scss"
 
 import {Controller} from "react-hook-form"
-import InputMask from 'react-input-mask'
-
-const PhoneInput = forwardRef(({value, onChange, className, ...rest}, ref) => {
-	return (
-		<InputMask
-			mask='1-999-999-9999'
-			maskChar={null}
-			value={value}
-			placeholder='1-516-395-4024'
-			onChange={onChange}
-			className={className}
-			ref={ref}
-			{...rest}>
-		</InputMask>
-	)
-})
-PhoneInput.displayName = "PhoneInput"
+import { PatternFormat } from 'react-number-format'
 
 const PhoneInputField = ({ label, id, control }) => {
 	return (
@@ -28,15 +11,18 @@ const PhoneInputField = ({ label, id, control }) => {
 				control={control}
 				name={id}
 				render={(
-					{ field: { value, ...rest },
+					{ field: { value, ref, ...rest },
 						fieldState: { error } }) => {
 					return (
-						<PhoneInput
+						<PatternFormat
 							id={id}
+							format="1 ### ### ####"
 							value={value}
+							getInputRef={ref}
+							placeholder='1-516-395-4024'
 							className={`customSelectClass ${error ? 'selectError' : ''}`}
-							{...rest}>
-						</PhoneInput>
+							{...rest}
+						/>
 					)
 				}}
 			/>
